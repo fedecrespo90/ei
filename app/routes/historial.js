65f28a5d9ffa59669e3dd5b351d363baede48453
Fede Crespo <fedecrespo90@gmail.com>
2015-11-24 12:57:44 -0300

    Prepare files to solve bug 21

--- app/routes/historial.js
+++ app/routes/historial.js
@@ -6,7 +6,77 @@ var Historial = function(db, everyone) {
 
   return Historial;
 };
+Historial.get = function(req, res, next){
+  DB.ClienteCuentaCorriente.find({ where: { cliente_id: req.params.cliente_id } }).on('success', function(ff){
 
+  DB.Movimiento.findAll({
+        where: {'clienteCuentaCorriente.cuenta_corriente_id': ff.cuenta_corriente_id},
+	      include: [{model:DB.Vencimiento, include:[{model: DB.Cronograma}]},{model: DB.ClienteCuentaCorriente,
+	               include:[{  model:DB.Cliente
+	               }]
+	      }],
+	      order: '`movimiento`.`id` DESC'
+  }).on('success', function(a) {
+     var msg=[];
+     a.forEach(function(mov){
+        var tramite
+        if(mov.vencimiento_id == 0){
+          if(mov.ingreso){
+           tramite = "Ingreso "+mov.observacion
+          }else{
+            tramite = "Egreso "+mov.observacion
+          }
+        }else{
+           if(mov.tramite){
+              tramite="Tramite "+mov.observacion
+            }else{
+              var crono = " "
+              if(mov.vencimiento)
+                if(mov.vencimiento.cronograma)
+                  crono=" - "+mov.vencimiento.cronograma.mes+"/"+mov.vencimiento.cronograma.año+" - ";
+              if(mov.ingreso){
+                tramite="Cliente pagó al Estudio/Ingreso " + mov.observacion + crono
+                if(mov.vencimiento){
+                  if(mov.vencimiento.pagado==1){
+                    if(mov.vencimiento.grupo_impuesto_id!= null){
+                      if(mov.vencimiento.descargado!= null){
+                        if(mov.vencimiento.archivado!= null){
+                          tramite+= " Archivado"
+                        }else{
+                        tramite+= " A Archivar"
+                        }
+                      }else{
+                        tramite+=" A Descargar"
+                      }
+                    }else{
+                      tramite+=" Enviado a Pagar"
+                    }
+                  }else{
+                    tramite+= " Sin enviar"
+                  }
+                }else{
+                  tramite+= " Sin Detallar"
+                }
+              }else{
+                tramite="El Estudio pagó/Egreso " + mov.observacion
+              }
+            }
+        }
+        //MANDA LA DATA A Clientes -> Cuentas Corrientes
+	      msg.push({
+	         id: mov.id,
+	         clienteNombre: mov.clienteCuentaCorriente.cliente.nombre,
+	         fecha: moment(mov.created_at).format('DD/MM/YYYY'),
+	         tramite: tramite, //mov.tramite? "Trámite"+mov.observacion:"Pago de Impuestos"+mov.observacion,
+	         monto: mov.monto.toMoney(),
+	         ingreso: mov.ingreso
+        });
+     });
+     res.send(msg);
+   })//CIERRO LA CONSULTA DE RECIEN (ff)
+  });
+};
+/*
 Historial.get = function(req, res, next) {
 //a pagar en transito a archivar"a descargar"archivado
   DB.Vencimiento.findAll(
@@ -46,6 +116,6 @@ Historial.get = function(req, res, next) {
     })
     res.send(msg)
   })
-};
+};*/
 
 module.exports = Historial;
