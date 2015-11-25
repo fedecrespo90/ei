f1ab2efcff42ac25f10e63bfc80e667fa111eb64
Fede Crespo <fedecrespo90@gmail.com>
2015-11-25 09:28:05 -0300

    Update historial.js to solve bug 21

--- docs/files/bug21/historial.get.js
+++ docs/files/bug21/historial.get.js
@@ -1,12 +1,46 @@
-var DB, Everyone
-  , moment= require('moment');
-var Historial = function(db, everyone) {
-  DB = db;
-  Everyone = everyone;
-
-  return Historial;
+Historial.get = function(req, res, next) {
+//a pagar en transito a archivar"a descargar"archivado
+  DB.Vencimiento.findAll(
+          {order: 'vencimiento.created_at DESC',
+          where: {cliente_id: req.params.cliente_id },
+          include: [{model: DB.Impuesto},{model: DB.GrupoImpuesto}]//{model: DB.CronogramaImpuesto},
+  }).on('success', function(a){
+    var msg= [];
+    a.forEach(function(d){
+      var total= parseFloat(d.monto0)+parseFloat(d.monto1)+parseFloat(d.monto2)+parseFloat(d.monto3)+parseFloat(d.monto4)
+      var imp= d.impuesto.nombre
+      var fecha;
+      if(d.grupo_impuesto_id != null){//si no entra es que obligadamente no se pago
+        fecha= moment(d.grupoImpuesto.diaDePago).format("DD/MM/YYYY");
+        if(d.grupoImpuesto.pagado!=null){
+          if(d.descargado != null){//no esta archivado
+            if(d.archivado!= null){
+              imp+=" (Archivado)";
+            }else{
+              imp+=" (A Archivar)";//Descargar
+            }
+          }else{
+            imp+=" (A Descargar)";//Archivar
+          }
+        }else{
+          imp+=" (En Tránsito)";
+        }
+      }else{
+        imp+=" (A Pagar)";
+        fecha="Sin Definir";
+      }
+      msg.push({
+        fecha: fecha,
+        impuesto: imp,
+        monto:total.toMoney(),
+      })
+    })
+    res.send(msg)
+  })
 };
-Historial.get = function(req, res, next){
+
+// MI HISTORIAL.GET
+/*Historial.get = function(req, res, next){
   DB.ClienteCuentaCorriente.find({ where: { cliente_id: req.params.cliente_id } }).on('success', function(ff){
   DB.Movimiento.findAll({
         where: {'clienteCuentaCorriente.cuenta_corriente_id': ff.cuenta_corriente_id},
@@ -63,6 +97,4 @@ Historial.get = function(req, res, next){
      });
    })//CIERRO LA CONSULTA DE RECIEN (ff)
   });
-};
-
-module.exports = Historial;
+};*/
